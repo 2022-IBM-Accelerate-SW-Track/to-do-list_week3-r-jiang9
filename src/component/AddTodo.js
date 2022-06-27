2f1387bbbb38a72f254b9ff1b2f43ea4c9e811b1
Renee Jiang <reneejiang@Renees-MBP.fios-router.home>
2022-06-27 02:24:29 -0400

    due date pickable

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Button, TextField } from "@mui/material";
+import { DesktopDatePicker , LocalizationProvider} from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 
 class AddTodo extends Component {
   // Create a local react state of the this component with both content date property set to nothing.
@@ -7,18 +9,27 @@ class AddTodo extends Component {
     super();
     this.state = {
       content: "",
-      date: ""
+      date: "",
+      duedate: null
     };
   }
+
   // The handleChange function updates the react state with the new input value provided from the user and the current date/time.
   // "event" is the defined action a user takes. In this case, the event is triggered when the user types something
   // into the text field.
   handleChange = (event) => {
     this.setState({
       content: event.target.value,
-      date: Date().toLocaleString('en-US')
+      date: Date().toLocaleString('en-US'),
     });
   };
+
+  handleDate = (event) => {
+    this.setState({
+      content: event.target.value,
+      duedate: Date(event).toLocaleString('en-US')
+    })
+  }
   // The handleSubmit function collects the forms input and puts it into the react state.
   // event.preventDefault() is called to prevents default event behavior like refreshing the browser.
   // this.props.addTodo(this.state) passes the current state (or user input and current date/time) into the addTodo function defined
@@ -29,10 +40,17 @@ class AddTodo extends Component {
       this.props.addTodo(this.state);
       this.setState({
         content: "",
-        date: ""
+        date: "",
+        duedate: null
       });
     }
   };
+
+  changeDue = (event) => {
+    this.setState({
+      duedate: new Date(event).toLocaleDateString(),
+    })
+  }
   render() {
     return (
       // 1. When rendering a component, you can render as many elements as you like as long as it is wrapped inside
@@ -46,9 +64,18 @@ class AddTodo extends Component {
         <TextField
           label="Add New Item"
           variant="outlined"
-          onChange={this.handleChange}
+          onChange={this.handleDate}
           value={this.state.content}
         />
+        <LocalizationProvider dateAdapter={AdapterDateFns}>         
+       <DesktopDatePicker
+            id="new-item-date"
+            label="Due Date"
+            value={this.state.duedate}
+            onChange={this.changeDue} // change function name!!
+            renderInput={(params) => <TextField {...params} />}
+        />
+    </LocalizationProvider>
         <Button
           style={{ marginLeft: "10px" }}
           onClick={this.handleSubmit}
